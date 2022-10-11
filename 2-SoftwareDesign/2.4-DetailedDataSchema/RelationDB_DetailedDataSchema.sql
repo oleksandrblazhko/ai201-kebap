e3250fd5c3706d02cc777f2f8be96a9eff18cb33
Дмитро Кебап <79566277+Dima-Kebap@users.noreply.github.com>
2022-10-11 12:20:02 +0300

    Update RelationDB_DetailedDataSchema.sql

--- 2-SoftwareDesign/2.4-DetailedDataSchema/RelationDB_DetailedDataSchema.sql
+++ 2-SoftwareDesign/2.4-DetailedDataSchema/RelationDB_DetailedDataSchema.sql
@@ -1,9 +1,9 @@
 /* видалення таблиць з каскадним видаленням 
 можливих описів цілісності */
 drop table employee CASCADE CONSTRAINTS;
-drop table user CASCADE CONSTRAINTS;
+drop table my_user CASCADE CONSTRAINTS;
 drop table product CASCADE CONSTRAINTS;
-drop table order CASCADE CONSTRAINTS;
+drop table my_order CASCADE CONSTRAINTS;
 drop table order_product CASCADE CONSTRAINTS;
 drop table message CASCADE CONSTRAINTS;
 
@@ -13,12 +13,12 @@ drop table message CASCADE CONSTRAINTS;
  CREATE SEQUENCE s_order; --
  CREATE SEQUENCE s_product;   --
 
-CREATE TABLE user ( -- опис користувача
+CREATE TABLE my_user ( -- опис користувача
 	user_id INT, -- id користувача
 	full_name VARCHAR(100), -- ПІБ
 	phone VARCHAR(20), -- номер телефону
 	pseudonym VARCHAR(100), -- псевдонім
-	avatar BINARY(1), -- аватарка
+	avatar bytea -- аватарка
 );
 
 CREATE TABLE employee ( -- опис співробітника
@@ -36,7 +36,7 @@ CREATE TABLE message ( -- опис онлайн-повідомлень
 	sender_id INT, -- id відправника
 );
 
-CREATE TABLE order ( -- опис онлайн-повідомлень
+CREATE TABLE my_order ( -- опис онлайн-повідомлень
 	order_id INT, -- id замовлення
 	order_time TIME, -- час замовлення
 	delivery_time TIME, -- час доставки
@@ -53,20 +53,20 @@ CREATE TABLE product ( -- опис онлайн-повідомлень
 );
 
 CREATE TABLE order_product (
-			order_id INT REFERENCES order(order_id),
+			order_id INT REFERENCES my_order(order_id),
 			product_id INT REFERENCES product(product_id),
 			PRIMARY KEY (order_id, product_id)
 );
 
 
 ---------- обмеження первинних ключів
-ALTER TABLE user ADD CONSTRAINT user_pk
+ALTER TABLE my_user ADD CONSTRAINT user_pk
 PRIMARY KEY (user_id);
 ALTER TABLE employee ADD CONSTRAINT employee_pk
 PRIMARY KEY (employee_id);
 ALTER TABLE message ADD CONSTRAINT message_pk
 PRIMARY KEY (msg_id);
-ALTER TABLE order ADD CONSTRAINT order_pk
+ALTER TABLE my_order ADD CONSTRAINT order_pk
 PRIMARY KEY (id_order);
 ALTER TABLE product ADD CONSTRAINT product_pk
 PRIMARY KEY (id_product);
@@ -80,25 +80,25 @@ REFERENCES user(user_id);
 
 ALTER TABLE message ADD CONSTRAINT message_rec_fk
 FOREIGN KEY (receiver_id)
-REFERENCES user(user_id);
+REFERENCES my_user(user_id);
 ALTER TABLE message ADD CONSTRAINT message_sen_fk
 FOREIGN KEY (sender_id)
-REFERENCES user(user_id);
+REFERENCES my_user(user_id);
 
-ALTER TABLE order ADD CONSTRAINT order_us_fk
+ALTER TABLE my_order ADD CONSTRAINT order_us_fk
 FOREIGN KEY (user_id)
-REFERENCES user(user_id);
-ALTER TABLE order ADD CONSTRAINT order_emp_fk
+REFERENCES my_user(user_id);
+ALTER TABLE my_order ADD CONSTRAINT order_emp_fk
 FOREIGN KEY (employee_id)
 REFERENCES employee(employee_id);
 
 	
 -- обмеження вмісту стовпчиків таблиць
-ALTER TABLE user ADD CONSTRAINT pseudonym_template
+ALTER TABLE my_user ADD CONSTRAINT pseudonym_template
     CHECK (regexp_like(pseudonym, 
-	           '^(@[a-z][a-z0-9._-]*\.[a-z]{2,4})$'));
+	           '@[a-z0-9_-]+'));
 
-ALTER TABLE user ADD CONSTRAINT user_phone_template
+ALTER TABLE my_user ADD CONSTRAINT user_phone_template
     CHECK ( regexp_like(phone, 
 	          '^(\([0-9]{3}\))?[0-9]{3}-[0-9]{4}$'));
 			  
