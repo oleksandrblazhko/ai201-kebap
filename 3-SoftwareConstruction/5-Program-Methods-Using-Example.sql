b34fd4cce0ae0a900ef351d5bfce2b76c4f887c3
Дмитро Кебап <79566277+Dima-Kebap@users.noreply.github.com>
2022-10-31 19:04:01 +0200

    Update 5-Program-Methods-Using-Example.sql

--- 3-SoftwareConstruction/5-Program-Methods-Using-Example.sql
+++ 3-SoftwareConstruction/5-Program-Methods-Using-Example.sql
@@ -3,61 +3,56 @@ DECLARE
     pr1 Product;
 BEGIN 
     pr1:= NEW Product(1,'моршинська',19.6);
-	    dbms_output.put_line('get get_product_id: ' || wf1.get_product_id);
-	    dbms_output.put_line('get get_pname: ' || wf1.get_pname);
-      dbms_output.put_line('get get_price: ' || wf1.get_price);
+	    dbms_output.put_line('get get_product_id: ' || pr1.get_product_id);
+	    dbms_output.put_line('get get_pname: ' || pr1.get_pname);
+      dbms_output.put_line('get get_price: ' || pr1.get_price);
     pr1.display();
 END;
 /
 
-/* Приклад роботи з класами Product, My_order */
+/* Приклад роботи з класами Product, My_user */
 DECLARE 
     pr1 Product;
-	prs Products_List;
-    ord1 My_order;
+    us1 My_user;
 BEGIN 
     pr1:= Product(1,'моршинська',19.6);
-	prs:= Products_List(pr1)
-    ord1:= My_order(1, TO_TIME('17:25','HH:MM'), TO_TIME('6:23','HH:MM'),'адреса_доставки',4724.2, 1, 2, prs);
-    ord1.display();
+	pr1.display();
+    us1:= My_user(1, 'Прізвище Імя По_батькові', "+380815402635", '@user_pseudo');
+    us1.display();
 END;
 /
 
-
-/* Приклад роботи з класами Product, My_order , My_user */
+/* Приклад роботи з класами Product, My_user, Employee */
 DECLARE 
     pr1 Product;
-	pr2 Product;
-	prs Products_List;
-    ord1 My_order;
     us1 My_user;
+    emp1 Employee;
 BEGIN
     pr1:= Product(1,'моршинська',19.6);
-	pr1:= Product(2,'КокаКола',29.8);
-	prs:= Products_List(pr1,pr2)
-    ord1:= My_order(1, TO_TIME('17:25','HH:MM'), TO_TIME('6:23','HH:MM'),'адреса_доставки',4724.2, 1, 2, prs);
-	ord1.display();
-    us1 := My_user(1, 'Прізвище Імя По_батькові', "+380815402635", '@user_pseudo');
-    us1.display(); 
+	pr1.display();
+    us1:= My_user(1, 'Прізвище Імя По_батькові', "+380815402635", '@user_pseudo');
+    emp1 := Employee(3, 'менеджер', 52000, TO_DATE('18.02.2017','DD/MM/YYYY'), us1); 
+    emp1.display(); 
 END;
 /
 
-/* Приклад роботи з класами Product, My_order , My_user, Employee */
+/* Приклад роботи з класами Product, My_user, Employee, My_order */
 DECLARE 
     pr1 Product;
 	pr2 Product;
 	prs Products_List;
     ord1 My_order;
     us1 My_user;
+	us2 My_user;
     emp1 Employee;
 BEGIN 
     pr1:= Product(1,'моршинська',19.6);
-	pr1:= Product(2,'КокаКола',29.8);
+	pr2:= Product(2,'КокаКола',29.8);
 	prs:= Products_List(pr1,pr2)
-    ord1:= My_order(1, TO_TIME('17:25','HH:MM'), TO_TIME('6:23','HH:MM'),'адреса_доставки',4724.2, 1, 2, prs);
-	ord1.display();
-    us1 := My_user(1, 'Прізвище Імя По_батькові', "+380815402635", '@user_pseudo');
-    emp1 := Employee(3, 'менеджер', 52000, TO_DATE('18.02.2017','DD/MM/YYYY'), us1); 
-    emp1.display(); 
+    us1:= My_user(1, 'Прізвище Імя По_батькові', "+380815402635", '@user_pseudo');
+	us1:= My_user(2, 'Прізвище Імя По_батькові користувача 2', "+380016442635", '@user_pseudo2');
+    emp1 := Employee(3, 'менеджер', 52000, TO_DATE('18.02.2017','DD/MM/YYYY'), us2); 	
+    ord1:= My_order(1, TO_TIME('17:25','HH:MM'), TO_TIME('6:23','HH:MM'),'адреса_доставки',4724.2, us1, emp, prs);
+    ord1.display(); 
 END;
 /
